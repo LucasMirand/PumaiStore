70851c4d96a3fae884bf255537204f4bf7b98c21
lucas.miranda <lucasemiranda54@gmail.com>
2021-09-24 09:50:21 -0300

    clase12

--- src/componente/Cart/Cart.jsx
+++ src/componente/Cart/Cart.jsx
@@ -1,23 +1,65 @@
-
+import { useState } from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import { useAppContext } from '../Context/appContext'
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import { getFirestore } from '../../service/getFirebase';
+import firebase from "firebase";
+import 'firebase/firestore'
 
 
 export const Cart = () => {
 
+    const[formData,setFormData] = useState(initialState)
+
     const {producto,borrarListado,borrarProducto,precioTotal} = useAppContext()
-    console.log(producto.length)
+
 
     let carritoLleno = false
 
     if (producto.length === 0){
         carritoLleno=true
     }
-    console.log(carritoLleno)
 
+
+    function handleChange(e){
+        setFormData(
+            {
+                ...formData,
+                [e.target.name]:e.target.value
+            }
+        )
+        console.log(formData)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const newOrder={
+            buyer: formData,
+            items: producto,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: precioTotal()
+        }
+        console.log(newOrder)    
+        const db = getFirestore()
+        const orders = db.collection('orders')
     
+            //controlar si hay los productos que quiero agregar 
+        orders.add(newOrder)
+        .then(resp => alert(`la orden de compra es: ${resp.id}`))
+        .catch(err => console.log(err))
+        .finally(()=>{
+            setFormData(initialState)
+            borrarListado()
+        })
+        /*
+        db.collection('items').doc('idDelProd')
+        .update({
+            stock: stock-1
+        })
+        .then(res => console.log(res)) */
+    }
+
     return (
         <Container>
             
@@ -39,9 +81,29 @@ export const Cart = () => {
         </>:
         <>
         <div>Total: $ {precioTotal()}</div>
+        <br/>
+        <form 
+            onSubmit={handleSubmit}
+            onChange={handleChange}>
+                <input type="text" placeholder='ing nombre' name='nombre' value={formData.nombre}/>
+                <input type="text" placeholder='ing tel' name='tel' value={formData.tel} />
+                <input type="text" placeholder='ing email' name='email' value={formData.email} />
+                <input type="text" placeholder='confirme el mail' name='email2' />
+
+
+                <Button> Terminar compra</Button>
+        </form>
         <Link to='/'><Button>Volver a la tienda</Button></Link>
         <Button onClick={borrarListado}>Borrar Carrito</Button>
         </>}
+
+
         </Container>
     )
+}
+
+const initialState ={
+    nombre: '',
+    email:'',
+    tel:''
 }
